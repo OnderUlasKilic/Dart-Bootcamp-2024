de13450d3a7ec2d8344004608ebe6ba856d36796
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-01-16 00:30:23 +0300

    Add variable types

---
+++ 01_variable_types/homework_1.dart
@@ -0,0 +1,12 @@
+// yenni bir user adi oluştur
+// yeni bir para ver
+// bu parayı 20 ye böly ve ekrande göster
+
+void main() {
+  int ulasMoney = 40;
+  String userName = "Ulas";
+  ulasMoney = ulasMoney ~/ 20;
+
+  print("Merhaba $userName $ulasMoney");
+  //
+}
---
+++ 01_variable_types/start.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Bu kod ekranda mesajimi gosterir
+  print("Hello Nether");
+
+  print("Hello" + " Berkay");
+  print(2 + 3);
+
+// String verininAdi = "verinin degeri";
+
+  String hello = "Hello5";
+  String ahmet = "Hello5";
+
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+
+  int money = 25;
+  print(money);
+
+  print(hello + " " + "$money");
+}
