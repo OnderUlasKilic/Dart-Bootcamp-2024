46878be13215dac923b702fa0cc3b6a7024670d1
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-01-27 16:12:41 +0300

    Add join map singleWhere try catc

--- 13_list_advance/list_advance.dart
+++ 13_list_advance/list_advance.dart
@@ -38,9 +38,36 @@ void main(List<String> args) {
 
   final resultBmwMore20 = carItems.where((element) {
     return element.category == CarModels.bmw && element.money > 20;
-  });
+  }).join();
 
   print(resultBmwMore20);
+
+  final CarNames = carItems.map((e) => e.name).join(", ");
+  print(CarNames);
+
+  bool isHaveCarMercedes = false;
+
+  try {
+    final merdecesCar = carItems
+        .singleWhere((element) => element.category == CarModels.mercedes);
+    print(merdecesCar);
+    bool isHaveCarMercedes = true;
+  } catch (e) {
+    print("araba yok abi");
+    bool isHaveCarMercedes = false;
+  } finally {
+    print("abi bu işlem agır oldu bir daha sorma $isHaveCarMercedes");
+  }
+
+  final index = carItems.indexOf(newCar);
+  print(index);
+
+  carItems.add(
+      CarModel(category: CarModels.mercedes, name: "mercedes M5", money: 66));
+
+  carItems.sort((first, second) => first.money.compareTo(second.money));
+
+  print(carItems);
 }
 
 // benim bir arabalar sinifim olacak
@@ -53,6 +80,16 @@ void main(List<String> args) {
 // yeni bir araba geldi bu bizde var gibi hissediyorum dogrumu
 
 // bana bmw olan ve money 20 den büyük arabalarimizin ismini söylermisin
+
+// ya acaba sadece isimleri yan yana gösterirmisin
+
+// benim elimde mercedes varmı
+
+// su yeni gele arabaya var demiştin kacıncı sirada söylermisin
+
+// ya ben yeni araba aldım mercedes eklermisin
+
+// bana arabalarımı kücükten büyüge dogru sıralarmısın
 
 class CarModel {
   final CarModels category;
@@ -87,6 +124,11 @@ class CarModel {
         city.hashCode ^
         isSecondHand.hashCode;
   }
+
+  @override
+  String toString() {
+    return "$name - $money";
+  }
 }
 
-enum CarModels { bmw, yamaha, toyota }
+enum CarModels { bmw, yamaha, toyota, mercedes }
