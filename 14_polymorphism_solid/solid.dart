88c156d8d1a3f061e0286e0b86068894217ab5e3
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-01-30 22:57:42 +0300

    Add Liskov substitution Principle (LSP) Interface Segregation Principle (ISP) Dependency Inversion Principle (DIP)

--- 14_polymorphism_solid/solid.dart
+++ 14_polymorphism_solid/solid.dart
@@ -1,4 +1,9 @@
-void main() {}
+void main() {
+  IDatabase database = SQL();
+
+  database = Mongo();
+  database.write();
+}
 
 class UserManager {
   String name;
@@ -39,4 +44,64 @@ class ProductCategories extends Product {
   ProductCategories(String name, String money) : super(name, money);
 
   final String category = ";L";
+}
+
+abstract class IDatabase {
+  void write();
+}
+
+class SQL extends IDatabase {
+  @override
+  void write() {
+    // TODO: implement write
+  }
+}
+
+class Mongo extends IDatabase {
+  @override
+  void write() {
+    // TODO: implement write
+  }
+}
+
+//
+
+abstract class IUserOperation with IUserLocation, ILanguage {
+  void write();
+  void read();
+  void delete();
+}
+
+abstract mixin class IUserLocation {
+  void locationChange();
+}
+
+abstract mixin class ILanguage {
+  void language();
+}
+
+class UserLocation extends IUserLocation {
+  @override
+  void locationChange() {
+    // TODO: implement locationChange
+  }
+}
+
+abstract class ICameraManager {
+  void readQR();
+}
+
+class DeviceCameraManager extends ICameraManager {
+  final IphoneCameraRead iphoneCameraRead;
+
+  DeviceCameraManager(this.iphoneCameraRead);
+  @override
+  void readQR() {}
+}
+
+class IphoneCameraRead extends ICameraManager {
+  @override
+  void readQR() {
+    // TODO: implement readQR
+  }
 }
