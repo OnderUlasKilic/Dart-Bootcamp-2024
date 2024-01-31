a6f85f7f19d20b30c4d238a3b423aed88439338d
lordpeace12 <129948028+lordpeace12@users.noreply.github.com>
2024-01-31 21:11:20 +0300

    Add sync stream while exception custom exception

--- 15_asynch/asynch.dart
+++ 15_asynch/asynch.dart
@@ -6,21 +6,39 @@ Future<void> main() async {
   //   print("işlem bitti $element");
   // });
   // await Future.delayed(Duration(seconds: 2));
+  Stream<int> bankMoneys = Stream.empty();
+  bankMoneys = dataAddBankMoney(55, 3);
+  // bankMoneys.listen((event) {
+  //   print(event);
+  // });
+
+  print(await bankMoneys.where((event) => event == 258).toList());
 
   print("ab");
 
   print("hello");
-  Future.delayed(Duration(seconds: 2)).whenComplete(() {
+  Future.delayed(Duration(seconds: 1)).whenComplete(() {
     print("hello2");
   });
   print("hello3");
-  Future.delayed(Duration(seconds: 1)).whenComplete(() {
+  Future.delayed(Duration(seconds: 2)).whenComplete(() {
     print("hello4");
   });
 }
 
+Stream<int> dataAddBankMoney(int retryCount, int money) async* {
+  int _localRetry = 0;
+  await Future.delayed(Duration(seconds: 1));
+  while (_localRetry < retryCount) {
+    _localRetry++;
+    yield money += 5;
+    yield 5;
+  }
+  ;
+}
 
- // bir servise cevap sonradan gelecek
+
+// bir servise cevap sonradan gelecek
 // matematik işlemleri zaman alabilir
 
 // sync 
